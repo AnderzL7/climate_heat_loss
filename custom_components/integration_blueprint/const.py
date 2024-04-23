ef75cb1bd5c7884f474928eb2b2ce2e0343de37f
Anders Lund <a_lund_01@hotmail.com>
2024-04-23 15:15:44 +0200

    Renames all occurences of integration_blueprint

--- custom_components/integration_blueprint/const.py
+++ custom_components/integration_blueprint/const.py
@@ -1,9 +1,9 @@
-"""Constants for integration_blueprint."""
+"""Constants for climate_heat_loss."""
 from logging import Logger, getLogger
 
 LOGGER: Logger = getLogger(__package__)
 
-NAME = "Integration blueprint"
-DOMAIN = "integration_blueprint"
+NAME = "Climate with heat loss calculation"
+DOMAIN = "climate_heat_loss"
 VERSION = "0.0.0"
 ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
